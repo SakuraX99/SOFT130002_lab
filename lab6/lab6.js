30a4766eb959de659bcfaa89e88f74279ce909e5
TianyiZhang <17307110223@fudan.edu.cn>
2020-05-07 10:18:05 +0800

    Lab6

---
+++ lab6/lab6.js
@@ -0,0 +1,312 @@
+/*
+1.
+背景：
+    每隔五秒运行一次函数直到某一整分钟停止，比如从20:55:45运行到20:56:00停止；
+    或者运行10次，先到的为准。从1开始每过五秒，输入框内数值翻倍。初始值为1。
+注意：
+    你可以在函数 timeTest内部 和 timeTest外部 写代码使得该功能实现。
+要求：
+    ①要求使用JS闭包的方式使得计数实现局部私有，不可以在全局区域声明计数变量。
+    ②使用console.log打印计数即可，到达一分钟提前停止也需要console.log相应的提示语句。
+*/
+// testTime();
+
+
+
+function testTime(){
+    let count = (function () {
+        let counter = 1;
+        return function () {return counter *= 2;}
+    })();
+    let time = (function () {
+        let times = 0;
+        return function () {return times += 1;}
+    })();
+    let myDate = new Date();
+    let startTime = myDate.getMinutes();
+    console.log("count: " + 1);
+    let interval = setInterval(
+        function() {
+            let date = new Date();
+            if (date.getMinutes()==startTime && time() <= 10) {
+                console.log("count: " + count());
+            }
+            if(date.getMinutes()==startTime+1){
+                console.log("the next minute, stopped");
+                clearInterval(interval);
+            }
+        },5000)
+
+}
+// testTime();
+
+/*
+2.
+要求：
+    ①能够对传入的、移动手机电话（11位）、邮箱字符串（上网查找其要求）进行正则判定。
+    ②使用console.log打印即可，例如，电话不符合要求但是邮箱符合要求，则console.log("The telephone is right and the mail is wrong!")。
+    ③邮箱字符串的正则匹配的理解需写入lab文档。
+    ④telephone与mail均是字符串。
+*/
+// testMail("13908677486","ztianyi936@gmail.com");
+
+function testMail(telephone,mail) {
+    let telephonePattern = /^[1][3,5,7,8,9][0-9]{9}$/;
+    let mailPattern = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+    if(telephonePattern.test(telephone)){
+        if(mailPattern.test(mail)){
+            console.log("The telephone is right and the mail is right!");
+        }
+        else{
+            console.log("The telephone is right and the mail is wrong!");
+        }
+    }
+    else if(mailPattern.test(mail)){
+        console.log("The telephone is wrong and the mail is right!");
+    }
+    else{
+        console.log("The telephone is wrong and the mail is wrong!");
+    }
+}
+
+/*
+    1.以大写字母[A-Z]、小写字母[a-z]、数字[0-9]、下滑线[_]、减号[-]及点号[.]开头，并需要重复一次至多次[+]。
+    2.中间必须包括@符号。
+    3.@之后需要连接大写字母[A-Z]、小写字母[a-z]、数字[0-9]、下滑线[_]、减号[-]及点号[.]，并需要重复一次至多次[+]。
+    4.结尾必须是点号[.]连接2至4位的大小写字母[A-Za-z]{2,4}。
+ */
+
+/*
+3.
+要求：
+    ①输入一段全英文语句，要求使用正则表达式找到相邻的重复单词放入一个Set，如果集合中元素超过10个，则按照首字母顺序取前10个于集合。
+    ②使用console.log打印即可，将该集合打印出来。
+    ③例如：输入"Is is the iS is cost of of gasoline going up up"，输出：Set { 'Is is', 'iS is', 'of of', 'up up' }。
+    ④对该函数中用的正则匹配的理解需写入lab文档。
+    ⑤str为字符串。
+*/
+// testRedundancy("Is is the iS is cost of of gasoline going up up");
+function testRedundancy(str) {
+    let WORD_PATTERN = /\w+/ig;
+    let ret = str.match(WORD_PATTERN);
+    let map = new Map();
+    let duplication = "";
+    let count = 0;
+    let retSet = new Set();
+    for(let x = 0;x<ret.length;x++){
+        let s = ret[x].toLowerCase();
+        if(map.has(s)){
+            duplication+=" ";
+            duplication+=ret[x];
+            count++;
+        }
+        else{
+            if(count>=2){
+                retSet.add(duplication);
+            }
+            map.clear();
+            duplication = ret[x];
+            map.set(s,x);
+            count =1;
+        }
+    }
+    if(count>=2){
+        retSet.add(duplication);
+    }
+    console.log(retSet);
+}
+
+
+/*
+4.
+背景：
+    旧键盘上坏了几个键，于是在敲一段文字的时候，对应的字符就不会出现。
+    现在给出应该输入的一段文字、以及实际被输入的文字，请你使用Set列出肯定坏掉的那些键。
+    例如：输入7_This_is_a_test和_hs_s_a_es    输出：Set { '7', 'T', 'I' }
+要求：
+    ①需要使用Set。
+    ②只能使用一次循环。
+    ③使用console.log打印即可，将该集合打印出来。
+    ④wantInput和actualInput为字符串。
+注意：
+    ①注意联系生活，并注意观察我给的上述例子。
+*/
+// testKeyBoard("7_This_is_a_test","_hs_s_a_es");
+function testKeyBoard(wantInput, actualInput) {
+    let s = new Set();
+    for(let i=0,j = 0;i<wantInput.length;){
+        if(wantInput[i]==actualInput[j]){
+            i++;
+            j++;
+        }
+        else{
+            if(wantInput[i]<="z" && wantInput[i]>="a"){
+                let char = wantInput[i].toUpperCase();
+                s.add(char);
+            }
+            else{
+                s.add(wantInput[i])
+            }
+            i++;
+        }
+    }
+    console.log(s);
+}
+
+/*
+5.
+背景：
+    给定一个输入英文语句字符串，反转该语句。例如the sky is blue变成blue is sky the。
+要求：
+    ①如果输入的字符串前后有空格，输出中应该去除前后空格。如果输入字符串中间出现连续的两个空格，输出应该变为一个。
+    比如输入是“  hello  world!  ”，输出应该是“world! hello”。
+    ②请使用Array。
+    ③使用console.log打印即可，将该字符串打印出来。
+    ④只能显式使用一次循环。
+    ⑤str为字符串。
+*/
+// testSpecialReverse("  hello  world!  ");
+function testSpecialReverse(str) {
+    let words = str.split(" ");
+    words.reverse();
+    let ret = "";
+    for(let x = 0;x<words.length; x++){
+        if(words[x]!=""){
+        ret+=words[x];
+        ret+=" ";}
+    }
+    console.log(ret);
+}
+
+/*
+6.
+背景：
+    给定一个整数数组和一个值，找出相加为该值的两个元素下标并保存在一个数组中。
+    例如给定 [2, 7, 11, 15]和9,
+    打印结果为[0,1]
+要求：
+    ①使用Map。
+    ②只能显式使用一次循环。
+    ③使用console.log打印即可，将满足条件的数组打印出来。
+    ④nums为数字数组，如[1,2,3,4],target为数字,如5，那么输出为
+    [ 0, 3 ]
+    [ 1, 2 ]
+*/
+// twoSum([1,2,3,4],5);
+function twoSum(nums, target) {
+    let map = new Map();
+    for(let x = 0;x<nums.length;x++){
+        if(map.has(nums[x])){
+            let array = [map.get(nums[x]),x];
+            console.log(array);
+        }
+        else{
+            map.set(target-nums[x],x);
+        }
+    }
+}
+
+
+/*
+7.
+背景：
+    打印最长的包含不同字符串的子字符串长度。
+要求：
+    ①使用Map。
+    ②例如：输入"abbbbb",输出2，输入"bbbbb",输出1；
+    ③只能显式使用一次循环。
+    ④使用console.log打印即可。
+    ⑤str为字符串。
+*/
+// lengthOfLongestSubstring("bbbbb");
+function lengthOfLongestSubstring(str) {
+    let map = new Map();
+    let maxret = [0,0];
+    let maxlen = 0;
+    let start = 0;
+    for(let x = 0;x<str.length;x++){
+        if(map.has(str[x])){
+            let currentlen = map.size;
+            if(currentlen>maxlen){
+                maxlen = currentlen;
+                maxret[0] = start;
+                maxret[1] = x;
+            }
+            map.clear();
+            start = x;
+            map.set(str[x],x);
+        }
+        else{
+            map.set(str[x],x)
+        }
+    }
+    console.log(str.substring(maxret[0],maxret[1]));
+}
+
+/*
+8.
+背景：
+    该部分只是为了让你们自己动动手更好地感受不同继承方式。
+要求：
+    ①借助构造函数、原型链、和Object.create分别编写DevelopingCountry、PoorCountry、DevelopedCountry以实现对Country的继承，
+    并在三者分别添加sayHi、saySad、sayHappy函数分别打印"Hi,i am a developing country."、"I am a sad poor country."、"I am a Happy developed country."
+    ②请调用他们并打印相关语句即可。
+*/
+function Country() {
+    this.name = "国家";
+}
+//借助构造函数
+function DevelopingCountry() {
+    Country.call(this);
+    this.sayHi = function () {
+        console.log("Hi,i am a developing country.");
+    }
+}
+//原型链
+function PoorCountry() {
+    this.saySad = function () {
+        console.log("I am a sad poor country.");
+    }
+}
+
+PoorCountry.prototype = new Country();
+
+//寄生式继承
+function DevelopedCountry(original) {
+    let clone = Object(original);
+    clone.sayHappy= function () {
+        console.log("I am a Happy developed country.");
+    }
+    return clone;
+}
+
+function test() {
+
+    console.log("Q2----------------------")
+    testMail("13908677486","ztianyi936@gmail.com");
+    testMail("139086774ng","ztianyi936@gmail.com");
+    testMail("13908677486","ztianyi936@.com");
+    console.log("Q3----------------------")
+    testRedundancy("Is is the iS is cost of of gasoline going up up");
+    console.log("Q4----------------------")
+    testKeyBoard("7_This_is_a_test","_hs_s_a_es");
+    console.log("Q5----------------------")
+    testSpecialReverse("  hello  world!  ");
+    console.log("Q6----------------------")
+    twoSum([1,2,3,4],5);
+    console.log("Q7----------------------")
+    lengthOfLongestSubstring("abbbbcfrb");
+    console.log("Q8----------------------")
+    let country = new Country();
+
+    let country1 = new DevelopingCountry();
+    country1.sayHi();
+    let country2 = new PoorCountry();
+    country2.saySad();
+    let country3 = DevelopedCountry(country);
+    country3.sayHappy();
+    console.log("Q1----------------------")
+    testTime();
+}
+
+test();
