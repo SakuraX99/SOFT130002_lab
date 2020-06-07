d4b4b89dcff1a100b8aaaa7ba46e938f23129a5c
TianyiZhang <17307110223@fudan.edu.cn>
2020-06-07 11:16:25 +0800

    lab10

---
+++ lab10/config.php
@@ -0,0 +1,5 @@
+<?php define('DBHOST', 'localhost');
+define('DBNAME', 'art');
+define('DBUSER', 'root');
+define('DBPASS', '159753ztyzty');
+define('DBCONNSTRING','mysql:dbname=art;charset=utf8mb4;'); ?>
